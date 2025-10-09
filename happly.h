2a24827934ddbb2276e4eea2c88e472567fe171b
Simone Gasparini <simone.gasparini@gmail.com>
2025-10-09 15:46:46 +0200

    std::erase_if

--- happly.h
+++ happly.h
@@ -913,12 +913,7 @@ public:
     }
 
     // If there is already some property with this name, remove it
-    for (std::size_t i = 0; i < properties.size(); ++i) {
-      if (properties[i]->name == propertyName) {
-        properties.erase(properties.begin() + i);
-        i--;
-      }
-    }
+    std::erase_if(properties, [&propertyName](const auto& prop) { return prop->name == propertyName;});
 
     // Copy to canonical type. Often a no-op, but takes care of standardizing widths across platforms.
     std::vector<std::vector<typename CanonicalName<T>::type>> canonicalListVec;
